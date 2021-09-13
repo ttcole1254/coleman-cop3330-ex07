99826c5b648f74033785111dbb58a65867b1ef25
Test <Test@test.com>
2021-09-12 23:35:01 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,39 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 7 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner askLengthFeet = new Scanner(System.in);
+        System.out.println("What is the length of the room in feet? ");
+
+        String inputLengthFeetS = askLengthFeet.nextLine();
+        if(inputLengthFeetS.isEmpty()) {
+            System.out.println("Length of the room is required!");
+        }
+        else {
+            int inputLengthFeet = parseInt(inputLengthFeetS);
+            Scanner askWidthFeet = new Scanner(System.in);
+            System.out.println("What is the width of the room in feet? ");
+            String inputWidthFeetS = askWidthFeet.nextLine();
+            if(inputWidthFeetS.isEmpty()) {
+                System.out.println("Width of the room is required!");
+            }
+            else {
+                int inputWidthFeet = parseInt(inputWidthFeetS);
+                double areaFeet = inputLengthFeet * inputWidthFeet;
+                final double conversionFactor = 0.09290304;
+                double areaMeters = areaFeet * conversionFactor;
+                System.out.println("You entered dimensions of " + inputLengthFeet + " feet by " + inputWidthFeet + " feet.\nThe area is\n" + areaFeet + " square feet\n" + areaMeters + " square meters");
+            }
+        }
     }
 }
